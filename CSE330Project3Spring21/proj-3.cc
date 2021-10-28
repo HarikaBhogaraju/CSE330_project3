83d7c7ce29469894d083e9b33f448c4784262609
HarikaBhogaraju <harika.bhogaraju@gmail.com>
2021-10-27 20:14:13 -0700

    change

--- CSE330Project3Spring21/proj-3.cc
+++ CSE330Project3Spring21/proj-3.cc
@@ -8,18 +8,20 @@ int c = 0;
 int p = 0;
 int n = 0;
 
+int item = 0;
+
 void producer(){
   P(emptySem);
   buffer[in] = item;
 
-  in = (in+1)%N;
+  in = (in+1)%n;
   V(fullSem);
 }
 
 void consumer(){
   P(fullSem);
   item = buffer[out];
-  out = (out+1)%N;
+  out = (out+1)%n;
   V(emptySem);
 }
 
@@ -31,8 +33,5 @@ int main(){
   sem* mutexSem = initSem(1);
   int buffer[b];
 
-
-
-  int item = ;
   return 0;
 }
